4070269f07a2de677500061003d8c9c91c003b6e
jose lucas melo <jose.lucasdemelo@gmail.com>
2025-05-26 19:46:14 -0300

    added pagination

--- src/pages/Estudantes/components/StudentsTable/index.tsx
+++ src/pages/Estudantes/components/StudentsTable/index.tsx
@@ -1,11 +1,35 @@
 import { Table, Space } from "antd";
 import { Student } from "src/types";
+import type { AnyObject } from 'antd/es/_util/type';
+import type { SorterResult } from 'antd/es/table/interface';
+import type { GetProp, TableProps } from 'antd';
+import { useState } from "react";
+
+
+type TablePaginationConfig = Exclude<GetProp<TableProps, 'pagination'>, boolean>;
+
+interface TableParams {
+  pagination?: TablePaginationConfig;
+  sortField?: string;
+  sortOrder?: 'ascend' | 'descend';
+  // filters?: Record<string, FilterValue | null>;
+}
 
 interface StudentsTableProps {
   students: Student[];
 }
 
 export default function StudentsTable({ students }: StudentsTableProps) {
+  const [tableParams, setTableParams] = useState<TableParams>({
+    pagination: {
+      current: 1,
+      pageSize: 10,
+      showSizeChanger: true,
+      pageSizeOptions: ['10', '20', '50', '100'],
+      total: 0,
+    },
+  });
+  
   const columns = [
     { title: 'ID', dataIndex: 'id', key: 'id' },
     { title: 'RA', dataIndex: 'ra', key: 'ra' },
@@ -14,13 +38,30 @@ export default function StudentsTable({ students }: StudentsTableProps) {
     { title: 'Classe', dataIndex: 'classId', key: 'classId' },
   ];
 
+  const handleTableChange: TableProps<Student>['onChange'] = (
+    pagination,
+    filters,
+    sorter,
+    extra
+  ) => {
+    setTableParams({
+      pagination,
+    });
+  };
+
+
   return (
     <Space direction="vertical" size="middle" style={{ width: '100%' }}>
-      <Table 
+      <Table<Student>
         dataSource={students} 
         columns={columns} 
         rowKey="id"
-        pagination={{ pageSize: 10 }}
+        pagination={{
+          ...tableParams.pagination,
+          total: students.length,
+        }}
+        onChange={handleTableChange}
+        scroll={{ x: true }}
       />
     </Space>
   );
